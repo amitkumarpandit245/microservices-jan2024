ac2a99dc2337106534ad5285c21cbcbcc4c26021
vishymails <vishymails@gmail.com>
2024-01-11 15:41:54 +0530

    added

---
+++ actuator-service/src/main/java/com/bvr/AccuatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccuatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccuatorServiceApplication.class, args);
+	}
+
+}
